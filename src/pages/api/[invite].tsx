68982a92cf647fc3fcb923e67e83c8bb36c1892e
doceazedo <me@doceazedo.com>
2023-06-15 15:21:26 -0300

    Update error image style

--- src/pages/api/[invite].tsx
+++ src/pages/api/[invite].tsx
@@ -190,10 +190,29 @@ export default async function handler(req: NextRequest) {
 }
 
 const errorImage = (error: string) =>
-  new ImageResponse(<>{error}</>, {
-    width: 432,
-    height: 110,
-  });
+  new ImageResponse(
+    (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          height: "100%",
+          width: "100%",
+          padding: 16,
+          backgroundColor: "#ffffff",
+          color: "#ff0000",
+        }}
+      >
+        {error}
+      </div>
+    ),
+    {
+      width: 432,
+      height: 110,
+    }
+  );
 
 const getInviteData = async (invite: string) => {
   try {
